6611b2203dac820fa3ecff51e95a3b911c5b4ec1
Aviahelm <71475215+Aviahelm@users.noreply.github.com>
2025-09-30 11:22:48 +1000

    Quartz sync: Sep 30, 2025, 11:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://wyrda.pages.dev/",
+    baseUrl: "wyrda.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
